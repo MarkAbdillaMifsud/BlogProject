bb45e2ba5f6717f5efd9ba6c89298811db1e7291
Mark Abdilla Mifsud <60700422+MarkAbdillaMifsud@users.noreply.github.com>
2025-10-19 21:01:48 +0200

    Initial Server Setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let posts = [];
+
+app.get('/', (req, res) => {
+    res.render('index', { posts });
+});
+
+app.listen(PORT, () => console.log(`https://localhost:${PORT}`));
